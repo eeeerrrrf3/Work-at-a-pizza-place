57bc34ef8213d46c0f9f683c7c837b03f44bfac8
eeeerrrrf3 <108158864+eeeerrrrf3@users.noreply.github.com>
2023-01-02 17:10:59 +0300

    Update Work at a pizza place.lua

--- Work at a pizza place.lua
+++ Work at a pizza place.lua
@@ -1071,3 +1071,27 @@ v.Lifetime = NumberRange.new(0)
  end
 end
 end)
+
+local CreditsTab = Window:NewTab("Credits")
+local CreditsSection = CreditsTab:NewSection("Credits")
+CreditsSection:NewLabel("Made by Toipik#4001")
+
+CreditsSection:NewButton("Join Discord", "Joins the discord server.", function()
+    local http = game:GetService('HttpService') 
+    local req = syn and syn.request or http and http.request or http_request or fluxus and fluxus.request or getgenv().request or request
+	if req then
+		req({
+			Url = 'http://127.0.0.1:6463/rpc?v=1',
+			Method = 'POST',
+			Headers = {
+				['Content-Type'] = 'application/json',
+				Origin = 'https://discord.com'
+			},
+			Body = http:JSONEncode({
+				cmd = 'INVITE_BROWSER',
+				nonce = http:GenerateGUID(false),
+				args = {code = 'mfAjWaz2j9'}
+			})
+		})
+	end
+end)
